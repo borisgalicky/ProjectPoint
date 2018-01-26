94d636bc0df7128fb0db1cc1f6452fb1f8aed0c6
borisgalicky <boris.galicky@akademiasovy.sk>
2018-01-26 12:38:32 +0100

    added enum Gender, function Gender (not all is complete, at saturday it will be)

--- src/sk/akademiasovy/other/MyClass.java
+++ src/sk/akademiasovy/other/MyClass.java
@@ -7,6 +7,36 @@ public class MyClass{
     }
 
     public boolean isEmailValid(String email){
+        if(!email.contains("@"))
+            return false;
+        if(email.length()<5)
+            return false;
+        int i;
+        int countOfAt=0;
+        for(i=0;i<email.length();i++){
+            Character z=email.charAt(i);
+            if(Character.isAlphabetic(z) || Character.isDigit(z) || "-@._".contains(z.toString())){
+                // znak je platny
+                if(z.charValue()=='@')
+                    countOfAt++;
+                if(countOfAt==2)
+                    return false;
+            }
+            else{
+                return false;
+            }
+        }
 
+    public Gender getGender(String nin){
+        char z=nin.charAt(2);
+        if(z=="1"||z=="0"){
+            return Gender.MALE;
+        }
+        else if(z=="5" || z=="6"){
+            return Gender.FEMALE;
+        }
+        else{
+            return Gender.NONE;
+        }
     }
 }
